162f72eadda51131722bf0137e7501aeb5ad53e7
Igor Spasić <igor@jodd.org>
2019-02-21 00:27:22 +0100

    v1

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,20 +1,16 @@
-import axios from 'axios';
+import Fuse from 'fuse.js';
+const data = require('./data');
 
-// get the element
-const text = document.querySelector('.typing-text');
 
-// make a words array
-const words = [
+// words array
+var words = [
   "bla.",
   "bla bla.",
   "bla bla, igor.",
 ];
+var wordIndex = 0;
 
-// start typing effect
-setTyper(text, words);
-
-function setTyper(element, words) {
-
+function applyTyper(element) {
   const LETTER_TYPE_DELAY = 75;
   const WORD_STAY_DELAY = 2000;
 
@@ -22,7 +18,6 @@ function setTyper(element, words) {
   const DIRECTION_BACKWARDS = 1;
 
   var direction = DIRECTION_FORWARDS;
-  var wordIndex = 0;
   var letterIndex = 0;
 
   var wordTypeInterval;
@@ -54,12 +49,10 @@ function setTyper(element, words) {
     }
 
     const textToType = word.substring(0, letterIndex);
-
     element.textContent = textToType;
   }
 
   function nextWord() {
-
     letterIndex = 0;
     direction = DIRECTION_FORWARDS;
     wordIndex++;
@@ -67,6 +60,89 @@ function setTyper(element, words) {
     if (wordIndex == words.length) {
       wordIndex = 0;
     }
+  }
+}
 
+
+// typing
+const text = document.querySelector('.typing-text');
+applyTyper(text);
+
+// box
+const box = document.querySelector('#box');
+const unbox = document.querySelector('#unbox');
+const input = document.querySelector('#in');
+
+function boxStart() {
+  box.style.display = 'none';
+  unbox.style.display = 'flex';
+  input.value = "";
+  input.focus();
+}
+function boxEnd() {
+  box.style.display = 'block';
+  unbox.style.display = 'none';
+}
+
+box.addEventListener('click', function() {boxStart();});
+
+// keyboard
+
+document.onkeydown = function(evt) {
+    evt = evt || window.event;
+    var isEscape = false;
+    if ("key" in evt) {
+        isEscape = (evt.key === "Escape" || evt.key === "Esc");
+    } else {
+        isEscape = (evt.keyCode === 27);
+    }
+    if (isEscape) {boxEnd();}
+};
+
+
+// keyboard timeout
+var timeout = null;
+input.onkeyup = function (e) {
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      blabla(input.value);
+    }, 800);
+};
+
+
+// search
+
+var fuseOptions = {
+  shouldSort: true,
+  includeMatches: true,
+  threshold: 0.0,
+  tokenize:true,
+  location: 0,
+  distance: 100,
+  maxPatternLength: 32,
+  minMatchCharLength: 1,
+  keys: [
+    {name:"title",weight:0.8},
+    {name:"contents",weight:0.5},
+    {name:"tags",weight:0.3},
+    {name:"categories",weight:0.3}
+  ]
+};
+const f = new Fuse(data.site(), fuseOptions);
+
+function blabla(text) {
+  var results = f.search(text);
+  console.log(results);
+  if (results.length === 0) {
+    wordIndex = 0;
+    words = ['bla.', 'bla bla.', 'bla bla,m igor.'];
+  } else {
+    const arr = [];
+    for (var ndx in results) {
+      arr.push(results[ndx].item.title);
+    }
+    console.log(arr);
+    wordIndex = 0;
+    words = arr;
   }
 }
